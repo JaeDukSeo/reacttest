2cb39363ad144296ede3d59cebfa5b708a1e986e
Jae <jae.duk.seo@ryerson.ca>
2020-08-26 21:52:53 +0900

    Updates

---
+++ precache-manifest.90569b4191f5995cedec9c08ef0b8be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e993b5505bce20c65aec34aee37410",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "40f28879cce6a98aaf67",
+    "url": "/reacttest/static/css/main.3a9425a2.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/reacttest/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttest/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40f28879cce6a98aaf67",
+    "url": "/reacttest/static/js/main.5de50e1a.chunk.js"
+  },
+  {
+    "revision": "c49f2a31539d9a159379",
+    "url": "/reacttest/static/js/runtime-main.f58dcb93.js"
+  }
+]);
